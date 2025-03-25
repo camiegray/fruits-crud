d5cd477c7291586a5958800ae5e606dac79227fb
camiegray <camieg4@gmail.com>
2025-03-25 10:39:37 -0700

    Update final

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import "./db/connection.js";
-
+import methodOverride from "method-override"
 import fruitsRouter from "./controllers/fruits.js";
-
+import morgan from "morgan"
 const PORT = process.env.PORT || "3000";
 const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
 app.set("view engine", "ejs");
 
 // Routes
